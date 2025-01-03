42888297c99ed75841ca0113a63525b30727d533
Dziadzior <patrykroszkowski2020@gmail.com>
2025-01-03 20:18:32 +0100

    Poprawki

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'budzetownik-cache-v5';
+const CACHE_NAME = 'budzetownik-cache-vr4';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,34 +6,24 @@ const urlsToCache = [
   './script.js',
   './manifest.json',
   './web-app-manifest-192x192.png',
-  './web-app-manifest-512x512.png',
+  './web-app-manifest-512x512.png'
 ];
 
-// Instalacja Service Workera i dodanie plików do cache
+// Instalacja Service Workera
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Otwieranie cache i dodawanie plików');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Przechwytywanie żądań i serwowanie z cache
+// Pobieranie danych z cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).catch(() => {
-          console.error('Nie udało się pobrać zasobu:', event.request.url);
-        })
-      );
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
 
-// Usuwanie starych wersji cache przy aktywacji nowego Service Workera
+// Aktualizacja cache
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -41,7 +31,6 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Usuwanie starego cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -50,10 +39,9 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Obsługa notyfikacji push
+// Obsługa powiadomień push
 self.addEventListener('push', (event) => {
   const data = event.data.json();
-  console.log('Otrzymano powiadomienie push:', data);
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: './web-app-manifest-192x192.png',
